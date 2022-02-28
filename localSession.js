f4e4514b2a4c50ce61ae581f26e19be305b96b8e
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-02-28 12:33:03 +0545

    last commit

--- localSession.js
+++ localSession.js
@@ -1,12 +1,17 @@
 
-const arr=[ 'name','student', 'car','age'];
-localStorage.setItem('arr',JSON.stringify(arr));
-const getthat = JSON.parse( localStorage.getItem('arr'));
-document.getElementById('firs').innerHTML=getthat;
+// const arr=[ 'name','student', 'car','age'];
+// localStorage.setItem('arr',JSON.stringify(arr));
+// const getthat = JSON.parse( localStorage.getItem('arr'));
+// document.getElementById('firs').innerHTML=getthat;
 
-let dummyh=document.getElementById('firsth1');
-dummyh=[];
-localStorage.setItem('dummyh',JSON.stringify(dummyh));
-JSON.parse(localStorage.getItem(dummyh));
- 
+let dummyh=document.getElementById('firstinput');
+const mybtn=document.getElementById('firs_button')
+mybtn.addEventListener('click',myfunction)
+function myfunction()
+{
+localStorage.setItem('dummyh',dummyh.value);
+ let myvalue=localStorage.getItem('dummyh');
+document.getElementById('mylist').innerHTML=myvalue;
+console.log(myvalue);
+}
 
